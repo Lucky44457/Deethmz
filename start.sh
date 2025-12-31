9afa256c88e882eee417bf8505e287a22bc5ecda
Lucky44457 <maribelwate@gmail.com>
2025-12-31 12:19:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
-source mltbenv/bin/activate
-python3 update.py
-python3 -m bot
+#!/bin/bash
+# Make sure this script is executable: chmod +x start.sh
+
+# Run update.py using venv Python
+./mltbenv/bin/python3 update.py
+
+# Start the bot using venv Python
+./mltbenv/bin/python3 -m bot
